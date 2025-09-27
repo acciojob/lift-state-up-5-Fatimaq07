76d07f47d18b16cd767c3b0efc10e38141d1903b
FATIMA QURESHI <115686917+Fatimaq07@users.noreply.github.com>
2025-09-27 20:57:01 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,24 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
+import "./../styles/App.css";
+import Login from "./Login";
 
 const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleLogin = () => {
+    setIsLoggedIn(true);
+  };
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      {isLoggedIn ? (
+        <h2>Welcome! You are logged in ✅</h2>
+      ) : (
+        <Login onLogin={handleLogin} />
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
